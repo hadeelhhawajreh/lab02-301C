b79c13c872283771e75f28c0d6b60fcde3b55633
Hadeel H Al-Hawajreh <hadeelhhawajreh@gmail.com>
2020-10-21 03:04:08 +0300

    not solving all yet

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 let allAnimal = [];
-let optionArr = ['narwhal', 'narwhal', 'triceratops', 'rhino', 'mouflon', 'lizard', 'dragon', 'unicorn', 'markhor', 'chameleon', 'narwhal', 'narwhal', 'triceratops', 'rhino', 'mouflon', 'lizard', 'dragon', 'unicorn', 'markhor', 'chameleon'];
-
+let optionArr = ['narwhal', 'narwhal', 'giraffe', 'triceratops', 'rhino', 'mouflon', 'lizard', 'dragon', 'unicorn', 'markhor', 'chameleon', 'narwhal', 'narwhal', 'triceratops', 'rhino', 'mouflon', 'lizard', 'dragon', 'unicorn', 'markhor', 'chameleon'];
+var pageNumber='data/page-1.json';
 function Animal(animalObj) {
     this.title = animalObj.title;
     this.keyword = animalObj.keyword;
@@ -11,15 +11,30 @@ function Animal(animalObj) {
     allAnimal.push(this);
 }
 Animal.prototype.render = function () {
-    let templete = $('.photo-template').clone();
-    $('main').append(templete);
-    templete.find('h2').text(this.title);
-    templete.find('img').attr('src', this.image_url);
-    templete.find('p').text(this.description);
-    templete.removeClass('photo-template');
-    templete.attr('class', this.keyword);
+    // let templete = $('.photo-template').clone();
+    // $('main').append(templete);
+    // templete.find('h2').text(this.title);
+    // templete.find('img').attr('src', this.image_url);
+    // templete.find('p').text(this.description);
+    // templete.removeClass('photo-template');
+    // templete.attr('class', this.keyword);
+     let templete = $('.photo-template').html();
+     let sections=Mustache.render(templete,this);
+     $('main').append(sections);
 }
 
+function addSection(){
+    $('main').append(`  <template class="photo-template">
+
+    <section> //main container 
+      <h2>{{title}}</h2>
+      <img src="{{image_url}}" alt="">
+      <p>{{description}}</p>
+      <p>{{horns}}</p>
+    </section>
+
+  </template>`);
+}
 let uniqueKeyArr = [];
 optionArr.forEach((ele) => {
     if (!uniqueKeyArr.includes(ele)) {
@@ -49,9 +64,26 @@ Animal.readJson = () => {
                 animal_create.render();
             });
             selectItem();
+
+        });
+};
+Animal.readJson2 = () => {
+    const ajaxSettings = {
+        method: 'get',
+        dataType: 'json'
+    };
+    $.ajax('data/page-2.json', ajaxSettings)
+        .then(data => {
+            data.forEach(element => {
+                let animal_create = new Animal(element);
+                animal_create.render();
+            });
+            selectItem();
         });
 };
 $(() => Animal.readJson());
+$(() => Animal.readJson2());
+
 console.log('allAnimal');
 
 console.log(allAnimal);
@@ -61,7 +93,16 @@ $(document).ready(function () {
     function changeing(event) {
         let show = event.target.value;
         console.log(show);
-        $('div').hide();
+        $('main').hide();
         $(`.${show}`).fadeIn(1000);
     };
+});
+$(()=>{
+    $('#sort').click(function(){
+        allAnimal.sort((a,b)=>{
+            if(a.horns>b.horns)
+            return a-b;
+        });
+    });
+
 });
